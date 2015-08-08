5c75d98faf0758c29f1506f45eea2a749f902ee3
neoremind <notyetfish@hotmail.com>
2015-08-08 20:41:57 +0800

    del some unneccessray stuff

---
+++ navi-mgr-console/src/main/java/com/baidu/beidou/navimgr/auth/vo/BaseObject.java
@@ -0,0 +1,7 @@
+package com.baidu.beidou.navimgr.auth.vo;
+
+/**
+ * @author zhangxu
+ */
+public class BaseObject {
+}
---
+++ navi-mgr-console/src/main/java/com/baidu/beidou/navimgr/util/JsonUtils.java
@@ -0,0 +1,7 @@
+package com.baidu.beidou.navimgr.util;
+
+/**
+ * @author zhangxu
+ */
+public class JsonUtils {
+}
